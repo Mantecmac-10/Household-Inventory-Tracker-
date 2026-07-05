2c6fa17877aea860ee4014aea2fb86afa032150d
Mantecmac-10 <Mantech468@gmail.com>
2026-07-06 01:49:53 +0530

    Auth and Household completed in Home Inventory

--- backend/src/controllers/household.ts
+++ backend/src/controllers/household.ts
@@ -47,8 +47,89 @@ export const joinHoushold = async (
     const inviteCode = req.body.inviteCode;
     const userId = req.userId;
 
-    const houseHold = await household.findOne({ inviteCode });
+    const existhouseHold = await household.findOne({ inviteCode });
+    if (!existhouseHold) {
+      return res.status(400).json({ message: "Invalid Invite Code!" });
+    }
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json({ message: "User Doesn't Exists" });
+    }
+
+    const alreadyMember = existhouseHold.members.some(
+      (memberId) => memberId.toString() === user._id.toString(),
+    );
+    if (alreadyMember) {
+      return res
+        .status(400)
+        .send("User is already the member of this Household!");
+    }
+
+    existhouseHold.members.push(user._id);
+
+    await existhouseHold.save();
+
+    user.householdId = existhouseHold._id;
+
+    await user.save();
+
+    return res
+      .status(200)
+      .json({ message: "New User Added!", members: existhouseHold.members });
   } catch (error) {
     next(error);
+  }
+};
+
+export const getcurrentHousehold = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const userId = req.userId;
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(400).send("User Doesn't Exists");
+    }
+
+    const householdId = user.householdId;
+
+    const houseHold = await household.findById(householdId);
+
+    if (!houseHold) {
+      return res.status(400).send("Invalid ID");
+    }
+
+    return res
+      .status(200)
+      .send({ message: "Household of this User", houseHold });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMembers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const householdId = req.params.householdId;
+
+    const houseHold = await household.findById(householdId);
+
+    if (!houseHold) {
+      return res.status(400).send("Invalid Household ID!");
+    }
+
+    const result = houseHold.members;
+
+    return res.status(200).send({ message: "List of Members ", result });
+  } catch (error) {
+    next();
   }
 };
--- backend/src/routes/household.ts
+++ backend/src/routes/household.ts
@@ -1,7 +1,19 @@
 import { Router } from "express";
 import { verifyUser } from "../middlewares/auth";
+import {
+  createHousehold,
+  getcurrentHousehold,
+  getMembers,
+  joinHoushold,
+} from "../controllers/household";
 
+const router = Router();
 
-const router = Router()
+router.use(verifyUser);
 
-router.post('/',verifyUser,)
+router.post("/", createHousehold);
+router.post("/join", joinHoushold);
+router.get("/me", getcurrentHousehold);
+router.get("/:id/members", getMembers);
+
+export default router;
